310ab70ea7e1fe6fe9a5f6d18d2b0a2ad6a3a635
GowtherVM <pau.espinosa.fernandez@ieselcalamot.com>
2023-09-25 16:31:17 +0200

    inicio

---
+++ src/main/java/com/elcalamot/proyecto616/Proyecto616.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.elcalamot.proyecto616;
+
+/**
+ *
+ * @author Admin
+ */
+public class Proyecto616 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
